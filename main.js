2597c37a6e1dd60495e939b2efd6e756da10170a
shokhzodbek <samariddinovshokhzod@gmail.com>
2024-05-10 20:31:39 +0500

    12 ta masala

---
+++ main.js
@@ -0,0 +1,73 @@
+// Kiritish,Chiqarish va o'zlashtirih operatorlariga oid masalalar
+// 1-masala
+// let a = 4
+// let P = 4*a
+// console.log(P);
+// 2-masala
+// let a = 5
+// let S = a**2
+// console.log(S);
+// 3-masala
+// let a = 4;
+// let b = 5;
+// let S = a*b
+// let  P = 2*(a+b)
+// // 4-masala
+// let d = 3
+// const pi = 3.14
+// let L = pi*d
+// console.log(L);
+// 5 - masala
+// let a = 2
+// let V = a**3
+// let S = 6*a**2
+// console.log(S);
+// console.log(V);
+// 6-masala
+// let a, b, c;
+// a = 4;
+// b = 5;
+// c = 4;
+// let V= a*b*c
+// let S = 2*(a*b+b*c+a*c)
+// console.log(V);
+// console.log(S);
+// 7-masala
+// let R = 3
+// const pi = 3.14
+// let L = 2*pi*R
+// let S = pi*R**2
+// console.log(L);
+// console.log(S);
+// 8-masala
+// let a = 3
+// let b = 2
+// let arif = (a+b)/2
+// console.log(arif);
+// 9-masala
+// let a = 3;
+// let b = 4;
+// let geo = (a * b) ** 0.5;
+// console.log(geo);
+// 10-masala
+// let a = 4
+// let b = 5
+// let yig = a+b
+// let kup = a*b
+// let a1 = a**2
+// let b1 = b**2
+// console.log(yig,kup,a1,b1);
+// 11-masala
+// let a = 3
+// let b = 6
+// let yig = a+b
+// let kup = a*b
+// let a1 = Math.abs(a)
+// let b1 = Math.abs(b)
+// console.log(yig,kup,a1,b1);
+// 12-masala
+// let a = 2;
+// let b = 5;
+// let c = (a ** 2 + b ** 2) ** 0.5;
+// let P = a + b + c;
+// console.log(c, P);
